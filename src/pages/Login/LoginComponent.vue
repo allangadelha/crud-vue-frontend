3d0775efb1f174968a08945bcb0a8bc0505dc20f
allangadelha <allangadelha@gmail.com>
2022-07-11 10:38:46 -0300

    Update LoginComponent.vue

--- src/pages/Login/LoginComponent.vue
+++ src/pages/Login/LoginComponent.vue
@@ -41,7 +41,7 @@ export default {
                 email: this.LoginData.email,
                 password: this.LoginData.password
             }
-            axios.post('https://crud-vue-frontend.herokuapp.com/api/authenticate', data)
+            axios.post('https://crud-vue-backend.herokuapp.com/api/authenticate', data)
             .then((res) => {
                 var user = res.data.user;
                 var token = res.data.token;
