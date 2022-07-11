1564cb3b06339f1da13b522eda45d0a696f1d48e
allangadelha <allangadelha@gmail.com>
2022-07-10 21:13:16 -0300

    Update AddClientComponent.vue

--- src/pages/AddClient/AddClientComponent.vue
+++ src/pages/AddClient/AddClientComponent.vue
@@ -59,7 +59,7 @@ export default {
                 phone: this.dataClient.email,
                 address: this.dataClient.address,
             }
-            axios.post('http://localhost:3001/api/clients', data, {
+            axios.post('https://crud-vue-frontend.herokuapp.com/api/clients', data, {
                 headers: {
                     'Authorization': `Bearer ${this.token}`
                 }
